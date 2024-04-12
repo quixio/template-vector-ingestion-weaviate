8ad88d9aaa0c20972753ffdcfc1cc0f478e932d3
Merlin Carter <merlin+demo@quix.io>
2024-04-12 11:02:03 +0000

    Edited 'main.py'

--- Ingest to Weaviate Cloud VectorDB/main.py
+++ Ingest to Weaviate Cloud VectorDB/main.py
@@ -5,7 +5,7 @@ import os
 #  Initialize the Weaviate client. Replace the placeholder values with your actual Weaviate instance details.
 client = weaviate.Client(
     url=os.environ["weaviate_url"],
-    auth_client_secret=weaviate.AuthApiKey(api_key=os.environ["WEAVIATE_API KEY"]),
+    auth_client_secret=weaviate.AuthApiKey(api_key=os.environ["WEAVIATE_API_KEY"]),
     additional_headers={
         "X-OpenAI-Api-Key": os.environ["OPENAI_API_KEY"]
     }
